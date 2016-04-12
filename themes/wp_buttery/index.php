45353ac00b25ac47c6896f7100d7d9cd739dc01c
Zachary Lincoln <znlincoln@gmail.com>
2016-04-11 22:16:32 -0400

    posts detail page

--- themes/wp_buttery/index.php
+++ themes/wp_buttery/index.php
@@ -1,32 +1,32 @@
 <?php
 get_header();
 if ( have_posts() ) : ?>
-<section id="overview" class="journal">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="page-header">
-                    <h1>Journal</h1>
+    <section id="overview" class="journal">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="page-header">
+                        <h1>Journal</h1>
+                    </div>
                 </div>
             </div>
+            <div class="row">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <div class="col-xs-12 col-sm-6 col-md-4">
+                        <article class="post">
+                            <a href="<?php the_permalink(); ?>">
+                                <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>">
+                            </a>
+                            <span class="date"><?php the_time('m _ d _ y'); ?></span>
+                            <h1><?php the_title(); ?></h1>
+                            <?php the_field('excerpt'); ?>
+                            <a href="<?php the_permalink(); ?>" class="btn btn-link btn-block">Read More</a>
+                        </article>
+                    </div>
+                <?php endwhile; ?>
+            </div>
         </div>
-        <div class="row">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <div class="col-xs-12 col-sm-6 col-md-4">
-                    <article class="post">
-                        <a href="<?php the_permalink(); ?>">
-                            <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>">
-                        </a>
-                        <span class="date"><?php the_time('m _ d _ y'); ?></span>
-                        <h1><?php the_title(); ?></h1>
-                        <?php the_excerpt(); ?>
-                        <a href="<?php the_permalink(); ?>" class="btn btn-link btn-block">Read More</a>
-                    </article>
-                </div>
-            <?php endwhile; ?>
-        </div>
-    </div>
-</section>
+    </section>
 <?php endif;
 get_footer();
 
--- themes/wp_buttery/single-post.php
+++ themes/wp_buttery/single-post.php
@@ -1,59 +1,52 @@
-<?php get_header(); ?>
-<section id="detail" class="journal">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <nav class="navbar">
-                    <ul class="nav navbar-nav">
-                        <li><a href="{{ site.baseurl }}/journal/"><i class="glyphicon glyphicon-th"></i></a></li>
-                        <li><a href="#"><i class="glyphicon glyphicon-arrow-left"></i></a></li>
-                        <li><a href="#"><i class="glyphicon glyphicon-arrow-right"></i></a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-        <article class="row">
-            <div class="col-xs-4">
-                <div class="page-header">
-                    <h1>Title of Entry</h1>
-                    <span class="date">01 _ 02 _ 15</span>
-                    <h2>SUBHEAD?</h2>
+<?php
+get_header();
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <section id="detail" class="journal">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <nav class="navbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="<?php echo get_permalink(get_option('page_for_posts')); ?>"><i class="glyphicon glyphicon-th"></i></a></li>
+                            <li><a href="<?php echo get_permalink(get_next_post()->ID); ?>"><i class="glyphicon glyphicon-arrow-left"></i></a></li>
+                            <li><a href="<?php echo get_permalink(get_previous_post()->ID); ?>"><i class="glyphicon glyphicon-arrow-right"></i></a></li>
+                        </ul>
+                    </nav>
                 </div>
-                <p>Have you ever had 24 5-star rated craft beers in less than three days? We have. Checkout out our visual/verbal diary of 3 hazy spring days.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequuntur in porro blanditiis odit vel aliquid aperiam cupiditate, repellat vero labore voluptatibus sit reprehenderit illum, pariatur omnis molestiae eligendi praesentium.</p>
-                <a href="#">Share</a>
             </div>
-            <div class="col-xs-8">
-                <figure>
-                    <img class="img-responsive" src="http://placehold.it/1200x800">
-                    <figcaption><i>This is where captions for photo would go if necessary.  Two lines max is the rule.</figcaption>
-                </figure>
-                <figure>
-                    <img class="img-responsive" src="http://placehold.it/1200x800">
-                    <figcaption><i>This is where captions for photo would go if necessary.  Two lines max is the rule.</figcaption>
-                </figure>
-                <figure>
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <img class="img-responsive" src="http://placehold.it/1200x800">
-                        </div>
-                        <div class="col-xs-6">
-                            <img class="img-responsive" src="http://placehold.it/1200x800">
-                        </div>
+            <article class="row">
+                <div class="col-xs-4">
+                    <div class="page-header">
+                        <h1><?php the_title(); ?></h1>
+                        <span class="date"><?php the_time('m _ d _ y'); ?></span>
+                        <h2><?php the_field('subheading'); ?></h2>
                     </div>
-                    <figcaption><i>This is where captions for photos could go if necessary. Photos can be placed horizontally or vertically. Video can also be placed here. Two lines max is the rule.</figcaption>
-                </figure>
-            </div>
-        </article>
-    </div>
-</section>
-<?php get_footer();
+                    <?php the_content(); ?>
+                    <a href="#">Share</a>
+                </div>
+                <div class="col-xs-8">
+                    <figure>
+                        <img class="img-responsive" src="http://placehold.it/1200x800">
+                        <figcaption><i>This is where captions for photo would go if necessary.  Two lines max is the rule.</figcaption>
+                    </figure>
+                    <figure>
+                        <img class="img-responsive" src="http://placehold.it/1200x800">
+                        <figcaption><i>This is where captions for photo would go if necessary.  Two lines max is the rule.</figcaption>
+                    </figure>
+                    <figure>
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <img class="img-responsive" src="http://placehold.it/1200x800">
+                            </div>
+                            <div class="col-xs-6">
+                                <img class="img-responsive" src="http://placehold.it/1200x800">
+                            </div>
+                        </div>
+                        <figcaption><i>This is where captions for photos could go if necessary. Photos can be placed horizontally or vertically. Video can also be placed here. Two lines max is the rule.</figcaption>
+                    </figure>
+                </div>
+            </article>
+        </div>
+    </section>
+<?php endwhile; endif;
+get_footer();
